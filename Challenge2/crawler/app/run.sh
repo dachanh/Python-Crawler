40043bce4e62c4b3debcdabf3eaae71180d7ce57
dachanh <ghostcodert7@gmail.com>
2021-12-11 07:43:26 +0700

    update nginx conf

--- Challenge2/crawler/app/run.sh
+++ Challenge2/crawler/app/run.sh
@@ -1,6 +1,3 @@
 #!/bin/bash
-export FLASK_APP=main.py
-flask db init
-flask db migrate
-flask db upgrade
-gunicorn main:app --bind 0.0.0.0:8181 --workers 5
+
+gunicorn main:app --bind 0.0.0.0:8181 --workers 5 
